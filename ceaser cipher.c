f81ad5225cba2bb7eb72034aa69b4a33f61f2d55
ThatOneRachel <111395893+ThatOneRachel@users.noreply.github.com>
2022-08-21 13:49:53 -0300

    Add files via upload

---
+++ ceaser cipher.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 50
+int main ()
+{
+    char word[MAX], wordaux[MAX], c;
+    int i, shift, n, lenght;
+    printf ("Put your chosen word (in lowercase): ");
+    fgets (wordaux, MAX, stdin);
+    printf ("Put your number of shift: ");
+    scanf("%d", &shift);
+    lenght = strlen(wordaux) - 1; //size of the string without the '\0'
+    printf("The new word is: ");
+    for (i = 0; i < lenght; i++)
+    {
+        word[i] = wordaux[i] + shift;
+        if (word[i] > 'z')
+        {
+            n = word[i] - 123;
+            word[i] = 'a' + n;
+        }
+        if (wordaux[i] < 'a' || wordaux[i] > 'z')
+        {
+            word[i] = wordaux[i];
+        }
+        printf("%c", word[i]);
+    }
+    return 0;
+}
